5eb6bfaca0ff53eb56c8a37abd7557b008059036
Paul Brookes <paul.s.brookes@gmail.com>
2020-05-09 14:02:27 +0300

    Handle parameters as a dictionary during Monte-Carlo simulations.

--- pump/mc.py
+++ pump/mc.py
@@ -1,8 +1,63 @@
-from qutip import mcsolve, mcsolve, parallel_map, serial_map, Options, tensor, sigmam, qeye, destroy, sigmaz, steadystate
+from qutip import *
 from copy import deepcopy
 import numpy as np
-from .dynamic import c_ops_gen_jc, construct_ham
 import pandas as pd
+
+
+def construct_ham_floquet(params):
+    sz = tensor(sigmaz(), qeye(params['c_levels']))
+    sm = tensor(sigmam(), qeye(params['c_levels']))
+    a = tensor(qeye(2), destroy(params['c_levels']))
+
+    ham0 = (params['fc'] - params['fp']) * a.dag() * a + 0.5 * (params['fa'] - params['fp']) * sz
+    ham0 += params['g'] * (sm * a.dag() + sm.dag() * a)
+    ham0 += 0.5 * params['f1'] * (sm + sm.dag())
+    ham0 *= 2 * np.pi
+
+    return ham0
+
+
+def c_ops_gen_jc(params, alpha=0):
+    c_ops = []
+    sm = tensor(sigmam(), qeye(params['c_levels']))
+    a = tensor(qeye(2), destroy(params['c_levels'])) + alpha
+    if params['gamma'] > 0.0:
+        c_ops.append(np.sqrt(2 * np.pi * params['gamma'] * (1 + params['n_t'])) * sm)
+        if params['n_t'] > 0:
+            c_ops.append(np.sqrt(2 * np.pi * params['gamma'] * params['n_t']) * sm.dag())
+    if params['gamma_phi'] > 0.0:
+        c_ops.append(np.sqrt(2 * np.pi * params['gamma_phi']) * sm.dag() * sm)
+    if params['kappa'] > 0.0:
+        c_ops.append(np.sqrt(2 * np.pi * params['kappa'] * (1 + params['n_c'])) * a)
+        if params['n_c'] > 0:
+            c_ops.append(np.sqrt(2 * np.pi * params['kappa'] * params['n_c']) * a.dag())
+    return c_ops
+
+
+def ham1_coeff_rw(t, args):
+    params = args['params']
+    coeff = params['f2'] * np.exp(1j * 2 * np.pi * (params['fd'] - params['fp']) * t)
+    return coeff
+
+
+def ham2_coeff_rw(t, args):
+    params = args['params']
+    coeff = params['f2'] * np.exp(-1j * 2 * np.pi * (params['fd'] - params['fp']) * t)
+    return coeff
+
+
+def construct_ham(params):
+    a = tensor(qeye(2), destroy(params['c_levels']))
+
+    ham0 = construct_ham_floquet(params)
+
+    if params['f2'] is None or params['f2'] == 0:
+        return ham0
+    else:
+        ham1 = 2 * np.pi * a
+        ham2 = 2 * np.pi * a.dag()
+        H = [ham0, [ham1, ham1_coeff_rw], [ham2, ham2_coeff_rw]]
+        return H
 
 
 def generate_cut_mc(sweep_array, times, params, sweep_param='fd', psi0=None, parallel=False, num_cpus=10, opt=None,
@@ -12,7 +67,7 @@ def generate_cut_mc(sweep_array, times, params, sweep_param='fd', psi0=None, par
     params_list = []
     for value in sweep_array:
         params_copy = deepcopy(params)
-        setattr(params_copy, sweep_param, value)
+        params_copy[sweep_param] = value
         params_list.append(params_copy)
 
     if parallel:
@@ -37,9 +92,9 @@ def generate_result_mc(params, times, psi0=None, opt=None, e_ops=None, sweep_par
     args = {'params': params}
 
     if e_ops is None:
-        sm = tensor(sigmam(), qeye(params.c_levels))
-        sz = tensor(sigmaz(), qeye(params.c_levels))
-        a = tensor(qeye(2), destroy(params.c_levels))
+        sm = tensor(sigmam(), qeye(params['c_levels']))
+        sz = tensor(sigmaz(), qeye(params['c_levels']))
+        a = tensor(qeye(2), destroy(params['c_levels']))
         e_ops = {'a': a, 'sm': sm, 'n': a.dag() * a, 'sz': sz}
 
     c_ops = c_ops_gen_jc(params)
@@ -51,7 +106,7 @@ def generate_result_mc(params, times, psi0=None, opt=None, e_ops=None, sweep_par
         psi0 = states[-1]
 
     trace = mcsolve(H, psi0, times, c_ops, e_ops, args=args, options=opt, map_func=map_func)
-    measurement_frequency = params.fd - params.fp
+    measurement_frequency = params['fd'] - params['fp']
     trace.expect['a'] *= np.exp(1j * 2 * np.pi * measurement_frequency * times)
     trace.expect['sm'] *= np.exp(1j * 2 * np.pi * measurement_frequency * times)
 
@@ -60,11 +115,11 @@ def generate_result_mc(params, times, psi0=None, opt=None, e_ops=None, sweep_par
 
     if process:
         measurements = process_trace_mc(results_frame, params)
-        measurements[sweep_param] = getattr(params, sweep_param)
+        measurements[sweep_param] = params[sweep_param]
         measurements.set_index(sweep_param, inplace=True)
         return measurements
     else:
-        results_frame[sweep_param] = getattr(params, sweep_param)
+        results_frame[sweep_param] = params[sweep_param]
         results_frame.set_index(sweep_param, inplace=True, append=True)
         results_frame = results_frame.reorder_levels([sweep_param, 'time'])
         return results_frame
